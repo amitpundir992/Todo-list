f9889a4549fafa970ac127969c5f6309779e5b59
Amit <pundiramit06@gmail.com>
2023-02-26 12:26:07 +0530

    File updated

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-var container=document.querySelector(".container");
-var value1=document.querySelector(".input");
-var add1=document.querySelector(".add");
+var container = document.querySelector(".container");
+var value1 = document.querySelector(".input");
+var add1 = document.querySelector(".add");
 
-class item{
-    constructor(name)
-    {
-        this.create(name);
-    }
-    create(name)
-{
- var l1=document.createElement("div");
- l1.classList.add("item");
- 
- var input=document.createElement("input");
- input.type="text";
- input.disabled=true;
- input.value=name;
- input.classList.add("item_input");
+class item {
+  constructor(name) {
+    this.create(name);
+  }
+  create(name) {
+    var l1 = document.createElement("div");
+    l1.classList.add("item");
 
- var remove=document.createElement("button");
- remove.classList.add("remove");
- remove.innerHTML='<i class="fas fa-trash"></i>';
- remove.addEventListener('click',()=>this.remove(l1));
+    var input = document.createElement("input");
+    input.type = "text";
+    input.disabled = true;
+    input.value = name;
+    input.classList.add("item_input");
 
- container.appendChild(l1);
+    var remove = document.createElement("button");
+    remove.classList.add("remove");
+    remove.innerHTML = '<i class="fas fa-trash"></i>';
+    remove.addEventListener("click", () => this.remove(l1));
 
- l1.appendChild(input);
+    container.appendChild(l1);
 
- l1.appendChild(remove);
-}
+    l1.appendChild(input);
 
-remove(l1)
-{
+    l1.appendChild(remove);
+  }
+
+  remove(l1) {
     container.removeChild(l1);
+  }
 }
 
+add1.addEventListener("click", check);
+window.addEventListener("keydown", (e) => {
+  if (e.which == 13) {
+    check();
+  }
+});
+
+function check() {
+  if (value1.value != "") {
+    new item(value1.value);
+    value1.value = "";
+  }
 }
 
-add1.addEventListener('click',check);
-window.addEventListener("keydown",(e)=>{
-    if(e.which==13)
-    {
-        check();
-    }
-})
-
-function check()
-{
-    if(value1.value!="")
-    {
-        new item(value1.value);
-        value1.value="";
-    }
-
-}
-
-function removeall()
-{
-    document.querySelector(".container").innerHTML="";
+function removeall() {
+  document.querySelector(".container").innerHTML = "";
 }
